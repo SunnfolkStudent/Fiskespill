ada7891a37225e9394fa47411f3af85c95123e42
linear-allocator-abuser <rogopa6540@degcos.com>
2024-09-20 10:50:21 +0200

    Win and death calls

--- Assets/Scripts/Challenges/BigFish.cs
+++ Assets/Scripts/Challenges/BigFish.cs
@@ -1,30 +1,57 @@
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class BigFish : MonoBehaviour {
-	public float startingHealth = 100.0f;
+	public float maxHealth = 100.0f;
+	public float startingHealth = 50.0f;
+	public float healingSpeed = 0.1f;
 
 	[Space(10)]
-	public Vector2 startPosition;
-	public Vector2 endPosition;
+	public Vector2 maxHPPosition;
+	public Vector2 minHPPosition;
 
 	float health;
+	PlayerController player;
 
 	void Awake() {
 		health = startingHealth;
-		transform.position = new Vector3(startPosition.x, startPosition.y, 0.0f);
+		transform.position = new Vector3(maxHPPosition.x, maxHPPosition.y, 0.0f);
+
+		// We divide by maxHealth later.
+		Assert.AreNotEqual(maxHealth, 0.0f);
+
+		PlayerController[] players = Object.FindObjectsByType<PlayerController>(FindObjectsSortMode.None);
+		if (players.Length < 1) {
+			Debug.LogError("Found no player! Please create one.");
+			Application.Quit();
+		}
+
+		player = players[0];
 	}
 
     void FixedUpdate() {
-		// Put the big fish between the ending position and the starting position
-		var new_position = Vector2.Lerp(endPosition, startPosition, health / startingHealth);
+		health += healingSpeed;
+		health = Mathf.Clamp(health, 0.0f, maxHealth);
+
+		// Put the big fish between the max health position and the min health position
+		var new_position = Vector2.Lerp(minHPPosition, maxHPPosition, health / maxHealth);
 		transform.position = new Vector3(new_position.x, new_position.y, 0.0f);
+
+		if (health >= maxHealth) {
+			//player.GameOver();
+			print("GAME OVER!!!!!!");
+			Application.Quit();
+		} else if (health <= 0.0f) {
+			//player.YouWon();
+			print("YOU WON! :))))");
+			Application.Quit();
+		}
     }
 
 	void OnDrawGizmos() {
 		Gizmos.color = Color.blue;
-		Gizmos.DrawWireSphere(startPosition, 1.0f);
-		Gizmos.DrawWireSphere(endPosition,   1.0f);
-
-		Gizmos.DrawRay(startPosition, endPosition);
+		Gizmos.DrawWireSphere(maxHPPosition, 0.25f);
+		Gizmos.DrawWireSphere(minHPPosition, 0.25f);
+		Gizmos.DrawLine(maxHPPosition, minHPPosition);
 	}
 }
