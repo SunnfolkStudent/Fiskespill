4d38b1685160d498d9e3e53ac48bfad88f5fdc32
Drianna-79 <sofmolnet@gmail.com>
2024-09-20 11:43:49 +0200

    Input Update
    Added lines to make the player move

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,12 +6,20 @@ public class PlayerController : MonoBehaviour
     public float fishSize = 1f;
     
     private Rigidbody2D _rigidbody2D;
+    private Input_Actions _input;
     
     private void Start()
     {
+        _input = GetComponent<Input_Actions>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        _rigidbody2D.linearVelocityX = _input.Horizontal * swimSpeed;
+        _rigidbody2D.linearVelocityY = _input.Vertical * swimSpeed;
+    }
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Small Fish"))
